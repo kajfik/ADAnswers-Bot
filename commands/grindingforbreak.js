97d9186be35485b00ebb7c0b67e5122bbe63e470
L4R5W <66326188+L4R5W@users.noreply.github.com>
2021-04-23 16:11:02 +0200

    Exact numbers for max crunch buyers.
    Hellbach was to lazy, you were busy.
    And I like to help y'all and support your work.

--- commands/grindingforbreak.js
+++ commands/grindingforbreak.js
@@ -6,6 +6,6 @@ module.exports = {
   name: "grindingforbreak",
   description: "Describes how to reach Break Infinity.",
   execute(message) {
-    message.channel.send(`Get the 10,000 cost 2x IP then save up 30,000 (60,000 on web) IP and max your crunch autobuyer interval (you'll need to complete C12 if you haven't already)`);
+    message.channel.send(`Get the 10,000 cost 2x IP then save up. On mobile you will need a total of 32,767 IP (2^15 - 1). You'll have to make an additional purchase on web, because the crunch buyers default interval is at 300 seconds - while mobiles is at 150 seconds. That means you need 65,535 IP (2^16 - 1) in total if you're playing on web. (You'll also need to complete C12 if you haven't already)`);
   }
 };
