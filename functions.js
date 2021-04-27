998f8cdb05064051ba163fa80809afd0f04c11aa
earth <divine.icbm@gmail.com>
2021-04-26 20:29:28 -0500

    Finish function migration

--- functions.js
+++ functions.js
@@ -4,6 +4,17 @@
 
 const config = require("./config.json"); 
 
+const PRE_SPLIT_EARLY = "11,21,33,31,41";
+const PRE_SPLIT = "11,22,32,42,51,61";
+const EXTRA = "21,31,41,33,62";
+const ANTIMATTER = "71,81,91,101";
+const INFINITY = "72,82,92,102";
+const TIME = "73,83,93,103";
+const ACTIVE = "121,131,141";
+const PASSIVE = "122,132,142";
+const IDLE = "123,133,143";
+const POST_SPLIT = "151,161,171,181,162";
+
 function earlyGameCheck(id) {
   return config.ids.earlyGame.includes(id) || config.ids.common.includes(id) || config.ids.botCommands.includes(id);
 }
@@ -74,6 +85,100 @@ function toNumber(string) {
   return parseInt(string, 10);
 }
 
+Array.prototype.pushAll = function(...entries) {
+  entries.forEach(a => this.push(a));
+  return this;
+};
+
+function toPath(str = "active") {
+  switch (str.toLowerCase()) {
+  case "active": return ACTIVE;
+  case "passive": return PASSIVE;
+  case "idle": return IDLE;
+  default: throw `Unknown argument: Expected path name (\`active\`, \`passive\`, \`idle\`) but found: --> ${str} <--`;
+  }
+}
+
+function earlyEternity(theorem) {
+  if (theorem <= 13) {
+    return [PRE_SPLIT_EARLY];
+  }
+  if (theorem <= 39) {
+    return [PRE_SPLIT, ANTIMATTER, 21, 33];
+  }
+  if (theorem <= 44) {
+    return [PRE_SPLIT, INFINITY, 21, 33, 31];
+  }
+  if (theorem <= 51) {
+    return [PRE_SPLIT, ANTIMATTER, 111, 21, 33, 31];
+  }
+  return [PRE_SPLIT, INFINITY, 111];
+}
+
+function secondSplit(theorem, path) {
+  if (theorem <= 69) {
+    return [PRE_SPLIT, ANTIMATTER, 111, path, 21, 31];
+  }
+  if (theorem <= 70) {
+    return [PRE_SPLIT, INFINITY, 111, path, 21, 33, 31, 41];
+  }
+  if (theorem <= 84) {
+    return [PRE_SPLIT, ANTIMATTER, 111, path, 151, 161, 21, 33, 31];
+  }
+  if (theorem <= 99) {
+    return [PRE_SPLIT, INFINITY, 111, path, 151, 161, 162, 21, 33, 31];
+  }
+  return [PRE_SPLIT, TIME, 111, path, 151, 161, 171, 162, EXTRA];
+}
+
+function lightDark(theorem) {
+  const BASE = [PRE_SPLIT, TIME, 111, ACTIVE, POST_SPLIT, EXTRA];
+  if (theorem <= 1292) {
+    return BASE.pushAll(191, 212, 193, 214, 211, 213);
+  }
+  if (theorem <= 2142) {
+    return BASE.pushAll(193, 214, 228, 234);
+  }
+  if (theorem <= 2272) {
+    return BASE.pushAll(191, 212, 223, 232);
+  }
+  if (theorem <= 2692) {
+    return BASE.pushAll(191, 212, 223, 232, 193, 214, 211, 213);
+  }
+  if (theorem <= 3542) {
+    return BASE.pushAll(191, 211, 212, 223, 232, 192, 193, 214);
+  }
+  if (theorem <= 3712) {
+    return BASE.pushAll(191, 211, 222, 212, 224, 232, 193, 214);
+  }
+  if (theorem <= 3925) {
+    return BASE.pushAll(191, 212, 223, 232, 192, 201, INFINITY, 211, 193, 214, 213);
+  }
+  if (theorem <= 4945) {
+    return BASE.pushAll(191, 211, 222, 212, 224, 232, 192, 201, 72, 82, 92, 102, 193, 214, 213, 228);
+  }
+  return BASE.pushAll(191, 211, 222, 212, 224, 232, 192, 201, INFINITY, 193, 214, 228, 234, 213, 226);
+}
+
+function generateTree(theorem, path) {
+  let tree, desc = null;
+  if (theorem < 54) {
+    tree = earlyEternity(theorem);
+  } else if (theorem < 121) {
+    tree = secondSplit(theorem, path);
+  } else if (theorem < 318) {
+    tree = [PRE_SPLIT, TIME, 111, ACTIVE, POST_SPLIT, EXTRA];
+    desc = "At 900 Total TT, this flips over into EC10 territory.";
+  } else if (theorem < 901) {
+    tree = [PRE_SPLIT, TIME, 111, ACTIVE, POST_SPLIT, EXTRA, 191, 212, 211];
+    desc = "Do note: EC10 is done between this list and the previous.";
+  } else {
+    tree = lightDark(theorem);
+    desc = "If you cannot get the last TT to unlock dilation, use ++dilationgrind.";
+  }
+  return `${desc === null ? "" : `${desc} `}\`${tree.join(",")}|0\``;
+}
+
 module.exports = {
   earlyGameCheck,
   breakCheck,
@@ -86,6 +191,13 @@ module.exports = {
     getHelpDescription,
     getFooter,
     toNumber
+  },
+  studytree: {
+    toPath,
+    earlyEternity,
+    secondSplit,
+    lightDark,
+    generateTree
   },
   special: {
     bankedInfsCheck,
