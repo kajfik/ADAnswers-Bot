97e025b4d125d7a5e6e0c4406646a42b20de86f2
Buck4437 <Buck4437@users.noreply.github.com>
2021-05-02 01:10:58 +0800

    wording

--- commands/studytree.js
+++ commands/studytree.js
@@ -9,7 +9,7 @@ module.exports = {
   number: 4,
   name: "studytree",
   description: `Generates a Time Study tree based on your total Time Theorems.
-  Args: your total Time Theorems. Beginning at 54 TT (and until 120 TT), the command will take a second argument: \`active\`, \`passive\`, or \`idle\`. The argument is optional, and the default value is \`active\`.`,
+  Args: your total Time Theorems. Beginning at 54 TT, the command will take a second argument: \`active\`, \`passive\`, or \`idle\`. The argument is optional, and the default value is \`active\`.`,
   execute(message, args, id) {
     if (functions.special.studytreeCheck(id, message)) {
       try {
