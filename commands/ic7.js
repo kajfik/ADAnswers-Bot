698cc31fdc6415869dec9c4936b36fa6e3deaeff
L4R5W <66326188+L4R5W@users.noreply.github.com>
2021-05-04 09:19:17 +0200

    Create ic7.js

---
+++ commands/ic7.js
@@ -0,0 +1,13 @@
+/* eslint-disable max-len */
+"use strict";
+
+const { execute } = require("./challenge");
+
+module.exports = {
+  number: 5,
+  name: "ic7",
+  description: "shorthand for `++challenge ic7`",
+  execute(message) {
+    execute(message, ["ic7"], message.channel.id);
+  }
+};
