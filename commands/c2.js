26bd9b008fcf794b53270befa0aaf9b44d4c6cf0
earth <divine.icbm@gmail.com>
2021-05-04 07:05:41 -0500

    fix dum

--- commands/c2.js
+++ commands/c2.js
@@ -4,8 +4,8 @@
 const { execute } = require("./challenge");
 
 module.exports = {
-  number: 5,
-  name: "c7",
+  number: 7,
+  name: "c2",
   description: "shorthand for `++challenge c2`",
   execute(message) {
     execute(message, ["c2"], message.channel.id);
