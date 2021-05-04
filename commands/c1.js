7028cc7f5ecbcde32666e8fc02ddc1adaa0adc21
L4R5W <66326188+L4R5W@users.noreply.github.com>
2021-05-04 09:20:20 +0200

    Create c1.js

---
+++ commands/c1.js
@@ -0,0 +1,13 @@
+/* eslint-disable max-len */
+"use strict";
+
+const { execute } = require("./challenge");
+
+module.exports = {
+  number: 5,
+  name: "c1",
+  description: "shorthand for `++challenge c1`",
+  execute(message) {
+    execute(message, ["c1"], message.channel.id);
+  }
+};
