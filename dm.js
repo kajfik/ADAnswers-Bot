b5ce161814ffa39bcbf8fafbaeb68a04ac6abba8
L4R5W <66326188+L4R5W@users.noreply.github.com>
2021-05-13 20:20:42 +0200

    Create dm.js

---
+++ dm.js
@@ -0,0 +1,13 @@
+/* eslint-disable max-len */
+"use strict";
+
+module.exports = {
+  number: 7,
+  name: "dm",
+  description: "Yes, you can use the bot in your DM's with it.",
+  execute(message) {
+    message.channel.send(`Asking for `++ts`, `++ec`, and `++eco` will make the bot DM you instead of posting the response in the channel, to cut down on most of the spam.
+Make sure you have "receive DM's from server members" enabled in your privacy settings.
+If you want you can use all of the bot's commands directly in DM's with the bot.`);
+  }
+};
