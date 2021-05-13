84e88079b33df6c02f0c7fff0967398b0b163270
L4R5W <66326188+L4R5W@users.noreply.github.com>
2021-05-13 20:27:59 +0200

    Nvm I win this.

--- commands/dm.js
+++ commands/dm.js
@@ -6,7 +6,7 @@ module.exports = {
   name: "dm",
   description: "Yes, you can use the bot in your DM's with it.",
   execute(message) {
-    message.channel.send(`Asking for `++ts`, `++ec`, and `++eco` will make the bot DM you instead of posting the response in the channel, to cut down on most of the spam.
+    message.channel.send(`Asking for \`++ts\`, \`++ec\`, and \`++eco\` will make the bot DM you instead of posting the response in the channel, to cut down on most of the spam.
 Make sure you have "receive DM's from server members" enabled in your privacy settings.
 If you want you can use all of the bot's commands directly in DM's with the bot.`);
   }
