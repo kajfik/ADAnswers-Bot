c0d83e5dd1c000367b1ff24728110e9275d8fd05
earth <divine.icbm@gmail.com>
2021-05-15 13:49:17 -0500

    lars
    :angry:

--- commands/r133.js
+++ commands/r133.js
@@ -9,6 +9,6 @@ module.exports = {
   execute(message) {
     message.channel.send(`Due to eternity milestone 7 you beat ICs as soon as you unlock them. But you don't have the rewards in the first few ticks on a new eternity, because you still need to reach the amount of AM to unlock those ICs.
 This achievements grants you those rewards even if you haven't unlocked ICs yet.
-However, this is achievement reward is pretty much negligible as not having the IC rewards in the first 100ms won't border you that much.`);
+However, this is achievement reward is pretty much negligible as not having the IC rewards in the first 100ms won't hinder you that much.`);
   }
 };
