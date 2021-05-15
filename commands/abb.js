963602a834f7eb647d872da4e02d7ba3d74e6cf6
earth <divine.icbm@gmail.com>
2021-05-15 14:27:32 -0500

    Stuff lol

---
+++ commands/abb.js
@@ -0,0 +1,22 @@
+"use strict";
+
+module.exports = {
+  number: 7,
+  name: "abb",
+  description: "sends an abbreviation guide",
+  execute(message) {
+    message.channel.send(`AM - Antimatter
+    IP - ||Infinity Points||
+    EP - ||Eternity Points||
+    CX - Challenge X
+    ICX - ||Infinity Challenge X||
+    ECXxY - ||Eternity Challenge X completion Y||
+    IPow - ||Infinity Power||
+    IDX - ||Infinity Dimension X||
+    TDX - ||Time Dimension X||
+    TS - ||Time Study||
+    TT - ||Time Theorem||
+    DT - ||Dilated Time||
+    TP - ||Tachyon Particles||`);
+  }
+};
