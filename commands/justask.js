3ebe354e5524478fe1b96b9b7e8966fd566f76d4
L4R5W <66326188+L4R5W@users.noreply.github.com>
2021-05-18 17:54:53 +0200

    sorting

--- commands/justask.js
+++ commands/justask.js
@@ -4,7 +4,7 @@
 module.exports = {
   name: "justask",
   description: "sends a passive aggressive thing",
-  number: 6,
+  number: 7,
   execute(message) {
     message.channel.send("please just ask your question. don't ask to ask. don't ask for topic experts or DMs. don't ping random users. skip the formalities and ask away! https://dontasktoask.com/");
   }
