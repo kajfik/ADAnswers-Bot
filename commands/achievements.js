dc38e17d9b9bf35c687fc0cd20f38fae5d0357fe
L4R5W <66326188+L4R5W@users.noreply.github.com>
2021-05-18 17:30:14 +0200

    merged r133

--- commands/achievements.js
+++ commands/achievements.js
@@ -82,7 +82,11 @@ module.exports = {
       break;
     case "r133":
     case "inltisa":
-      message.channel.send(`Disable the autobuyer for the IP multiplier on the Infinity Upgrades (not Break Infinity Upgrades!) tab. See \`++r133\` on why the reward makes a difference.`);
+      message.channel.send(`Disable the autobuyer for the IP multiplier on the Infinity Upgrades (not Break Infinity Upgrades!) tab.
+
+Due to eternity milestone 7 you beat ICs as soon as you unlock them. But you don't have the rewards in the first few ticks on a new eternity, because you still need to reach the amount of AM to unlock those ICs.
+This achievements grants you those rewards even if you haven't unlocked ICs yet.
+However, this achievement reward is pretty much negligible as not having the IC rewards in the first ~100ms won't hinder you that much.`);
       break;
     case "r134":
     case "wwibe":
