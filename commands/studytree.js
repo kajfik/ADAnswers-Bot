991b0e03890842b0e644058f83582d4889381b08
L4R5W <66326188+L4R5W@users.noreply.github.com>
2021-05-18 17:36:41 +0200

    wrong sorting cause dumb

--- commands/studytree.js
+++ commands/studytree.js
@@ -7,7 +7,7 @@
 const functions = require("../functions");
 
 module.exports = {
-  number: 1,
+  number: 3,
   name: "studytree",
   description: `Has a shorthand: \`++ts\`. Generates a Time Study tree based on your total Time Theorems.
   Args: your total Time Theorems. Beginning at 54 TT, the command will take a second argument: \`active\`, \`passive\`, or \`idle\`. The argument is optional, and the default value is \`active\`.`,
