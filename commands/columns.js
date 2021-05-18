bdbcbfaa7e4ad685e07439927d0aa6293ad5b2a9
L4R5W <66326188+L4R5W@users.noreply.github.com>
2021-05-18 18:04:39 +0200

    sorting

--- commands/columns.js
+++ commands/columns.js
@@ -5,7 +5,7 @@ const functions = require("../functions");
 
 module.exports = {
   name: "columns",
-  number: 1,
+  number: 2,
   description: "sends an image with the columns of infinity upgrades",
   execute(message) {
     if (functions.earlyGameCheck(message.channel.id, message)) message.channel.send(`https://cdn.discordapp.com/attachments/822306768624287744/839301389452967957/Screenshot_20210505-103747_b29a8b237ccc9257831c4b60110b5dac__01.jpg`);
