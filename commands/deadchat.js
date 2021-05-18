e662d3e06efa5e740949d70098aecd45eb91d5ee
L4R5W <66326188+L4R5W@users.noreply.github.com>
2021-05-18 17:54:26 +0200

    sorting

--- commands/deadchat.js
+++ commands/deadchat.js
@@ -4,7 +4,7 @@
 module.exports = {
   name: "deadchat",
   description: "sends that one message from spec that he said that one time",
-  number: 6,
+  number: 7,
   execute(message) {
     message.channel.send(`If chat is quiet, just move on and do something else instead of annoying people with what effectively amounts to "hey someone talk already"`);
   }
