128e72a519fa0846cc92d89ffb6a1ee2a6093865
L4R5W <66326188+L4R5W@users.noreply.github.com>
2021-05-23 23:09:21 +0200

    added spoiler

--- commands/breakinfinity.js
+++ commands/breakinfinity.js
@@ -6,7 +6,7 @@ module.exports = {
   name: "breakinfinity",
   description: "describes break infinity and gives an order to get break infinity upgrades",
   execute(message) {
-    message.channel.send(`Break Infinity is unlocked by getting the Big Crunch autobuyer to its maximum interval of 0.10 seconds. When you Break Infinity, you are able to reach past 1.8e308 Antimatter. See more in the pins of the respective channel.
-    For the recommended upgrade order use \`++bugo\`.`);
+    message.channel.send(`Break Infinity is unlocked by getting the Big Crunch autobuyer to its maximum interval of 0.10 seconds. When you Break Infinity, ||you are able to get past 1.8e308 Antimatter||. See more in the pins of the respective channel.
+For the recommended upgrade order use \`++bugo\`.`);
   }
 };
