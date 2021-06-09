e1a1a1fe22996fe06c6d036599e3cb8f07a384a6
L4R5W <66326188+L4R5W@users.noreply.github.com>
2021-06-09 14:39:44 +0200

    Weeheeheehee dee heeheeheehee weeoh aweem away Weeheeheehee dee heeheeheehee weeoh aweem away In the jungle, the mighty jungle The lion sleeps tonight In the jungle the quiet jungle The lion sleeps tonight Wee heeheehee weeoh aweem away Wee heeheehee weeoh aweem away

--- commands/dm.js
+++ commands/dm.js
@@ -6,8 +6,9 @@ module.exports = {
   name: "dm",
   description: "Yes, you can use the bot in your DM's with it.",
   execute(message) {
-    message.channel.send(`Asking for \`++ts\`, \`++ec\`, and \`++eco\` will make the bot DM you instead of posting the response in the channel, to cut down on most of the spam.
-Make sure you have "receive DM's from server members" enabled in your privacy settings.
-If you want you can use all of the bot's commands directly in DM's with the bot.`);
+    message.channel.send(`You can use all of the bot's commands directly in DM's with the bot.
+    
+Asking for \`++ts\`, \`++ec\`, and \`++eco\` will make the bot DM you instead of posting the response in the channel, to cut down on most of the spam.
+Make sure you have "receive DM's from server members" enabled in your privacy settings.`);
   }
 };
