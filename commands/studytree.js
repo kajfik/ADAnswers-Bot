371427aa28b9d9565b1697a813e7707ae019df9a
earth <divine.icbm@gmail.com>
2021-06-28 21:45:17 -0500

    Bix sug

--- commands/studytree.js
+++ commands/studytree.js
@@ -15,11 +15,11 @@ module.exports = {
   Args: your total Time Theorems. Beginning at 54 TT, the command will take a second argument: \`active\`, \`passive\`, or \`idle\`. The argument is optional, and the default value is \`active\`.`,
     check: "studyTreeCheck",
     // Any number is only there to appear in the missing arg thing correctlyt
-    acceptableArgs: [["Any number"], ["active", "passive", "idle", ""]],
+    acceptableArgs: [["Any number"], ["active", "passive", "idle"]],
     sent: undefined,
     getArgMessage(args) {
       let theorem = 0;
-      let path = "";
+      let path = "active";
       // First arg
       if (!isNaN(functions.misc.toNumber(args[0]))) theorem = args[0];
       else return "Theorem amount provided is not a number";
@@ -27,6 +27,8 @@ module.exports = {
       if (args[1] === undefined) args[1] = path;
       if (this.acceptableArgs[1].includes(args[1].toLowerCase())) path = args[1].toLowerCase();
       else return "Path provided is not a path.";
+
+      path = functions.studytree.toPath(path);
       // Putting it together
       return functions.getMessage("ts", { theorem, path });
     }
