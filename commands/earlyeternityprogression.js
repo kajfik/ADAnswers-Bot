0054237bc594eab5ae61c635a93c7ebbc284db15
earth <divine.icbm@gmail.com>
2021-07-03 09:52:11 -0500

    Small addition

--- commands/earlyeternityprogression.js
+++ commands/earlyeternityprogression.js
@@ -14,6 +14,8 @@ module.exports = {
     2.  (Buying TS21 path + RESPEC) Eternity at e426IP for 3EP, spend all on TT
     3.  (Buy TS42) Eternity at e500IP for 4 EP
     (Buy TS51) Eternity at e614IP for 8EP
-    4.  Buy TS61, then get 100 eternities`]
+    4.  Buy TS61, then get 100 eternities.
+    
+    Afterwards, check out this flowchart (also pinned in eternity to ec1): https://media.discordapp.net/attachments/387799188848640013/860726060647841852/unknown.png`]
   })
 };
