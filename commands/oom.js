90215464ef39c5d4ed2a03d92a40be40e23d7c11
earth <divine.icbm@gmail.com>
2021-07-09 21:44:33 -0500

    dasdasd

---
+++ commands/oom.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const { Command } = require("../classes/Command");
+
+module.exports = {
+  command: new Command({
+    name: "oom",
+    number: 6,
+    check: true,
+    description: "describes what an OoM (Order of Magnitude) is",
+    acceptableArgs: undefined,
+    sent: [`An OoM (or **O**rder **o**f **M**agnitude) is the difference between the exponents of numbers, i.e. 1e100 -> 1e108 is a difference of 8 OoMs.`]
+  }),
+};
