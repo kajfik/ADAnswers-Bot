dd097a630ed296e67eb843c19f89a3743f26ced1
earth <divine.icbm@gmail.com>
2021-07-27 15:19:41 -0500

    add shit lul

---
+++ commands/challengecodes.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const { Command } = require("../classes/Command");
+
+module.exports = {
+  command: new Command({
+    name: "challengecodes",
+    description: "Sends a picture with all of the challenges notated with their \"code\"",
+    number: 2,
+    check: "earlyInfinity",
+    sent: [`Even though this image was originally made for the web version, it is still identical for mobile.
+    https://media.discordapp.net/attachments/387798906333036546/541018211215343616/C-Codes.png.`]
+  }),
+};
+
+// I eat ass
