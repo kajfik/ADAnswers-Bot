5fabbcd4049f01f1bae40a5c876f264ca97af6a9
earth <divine.icbm@gmail.com>
2021-08-07 22:45:07 -0500

    Begin dividing up functions.js
    Hopefully will push us back to A+ codefactor

--- utils/functions.js
+++ utils/functions.js
@@ -11,6 +11,7 @@
 const config = require("./config.json");
 const { MessageActionRow, MessageButton } = require("discord.js");
 const footerMessages = require("./footerMessages");
+const checks = require("./checks");
 
 const PRE_SPLIT_EARLY = "11,21,33,31,41";
 const PRE_SPLIT = "11,22,32,42,51,61";
@@ -22,168 +23,6 @@ const ACTIVE = "121,131,141";
 const PASSIVE = "122,132,142";
 const IDLE = "123,133,143";
 const POST_SPLIT = "151,161,171,181,162";
-
-/**
- * Checks if the ID specified is for early game channels.
- * @param {string} id the ID of the message channel.
- * @param {object} message object contains everything about the message.
- * @returns {Boolean}
- */
-function earlyGameCheck(id, message) {
-  return config.ids.earlyGame.includes(id) || config.ids.common.includes(id) || botCommandsCheck(id, message);
-}
-
-/**
- * Checks if the ID specified is for break infinity channels.
- * @param {string} id the ID of the message channel.
- * @param {object} message object contains everything about the message.
- * @returns {Boolean}
- */
-function breakCheck(id, message) {
-  return config.ids.break.includes(id) || config.ids.common.includes(id) || botCommandsCheck(id, message);
-}
-
-/**
- * Checks if the ID specified is for early eternity channels.
- * @param {string} id the ID of the message channel.
- * @param {object} message object contains everything about the message.
- * @returns {Boolean}
- */
-function earlyEternityCheck(id, message) {
-  return config.ids.earlyEternity.includes(id) || config.ids.common.includes(id) || botCommandsCheck(id, message);
-}
-
-/**
- * Checks if the ID specified is for EC channels.
- * @param {string} id the ID of the message channel.
- * @param {object} message object contains everything about the message.
- * @returns {Boolean}
- */
-function ecsCheck(id, message) {
-  return config.ids.ecs.includes(id) || config.ids.common.includes(id) || botCommandsCheck(id, message);
-}
-
-/**
- * Checks if the ID specified is for endgame channels
- * @param {string} id the ID of the message channel.
- * @param {object} message object contains everything about the message.
- * @returns {Boolean}
- */
-function endgameCheck(id, message) {
-  return config.ids.endgame.includes(id) || config.ids.common.includes(id) || botCommandsCheck(id, message);
-}
-
-/**
- * Checks if the ID specified is bot commands, #bots in earth's things, #general in bot test server, or if the command was used in DMs
- * @param {string} id the ID of the message channel.
- * @param {object} message object contains everything about the message.
- * @returns {Boolean}
- */
-function botCommandsCheck(id, message) {
-  // 603002159864348703 is #bots in Earth's things
-  // 722268615973273725 is #general in bot test server
-  return config.ids.botCommands.includes(id) || id === "603002159864348703" || id === "722268615973273725" || message.channel.type === "DM" || id === "873209506430066808";
-}
-
-/**
- * Checks if the ID specified is for common channels
- * @param {string} id the ID of the message channel.
- * @param {object} message object contains everything about the message.
- * @returns {Boolean}
- */
-function commonCheck(id) {
-  return config.ids.common.includes(id);
-}
-
-/**
- * Checks if the ID specified matches the correct channels for banked infinities
- * @param {string} id the ID of the message channel.
- * @param {object} message object contains everything about the message.
- * @returns {Boolean}
- */
-function bankedInfsCheck(id, message) {
-  return config.ids.common.includes(id) || config.ids.ecs[1] === id || config.ids.endgame.includes(id) || botCommandsCheck(id, message);
-}
-
-/**
- * Checks if the ID specified matches the correct channels for dilation grind
- * @param {string} id the ID of the message channel.
- * @param {object} message object contains everything about the message.
- * @returns {Boolean}
- */
-function dilationGrindCheck(id, message) {
-  return config.ids.endgame.includes(id) || config.ids.common.includes(id) || botCommandsCheck(id, message) || config.ids.ecs[1] === id;
-}
-
-/**
- * Checks if the ID specified is for early infinity channels
- * @param {string} id the ID of the message channel.
- * @param {object} message object contains everything about the message.
- * @returns {Boolean}
- */
-function earlyInfinityCheck(id, message) {
-  return config.ids.earlyGame[1] === id || config.ids.common.includes(id) || botCommandsCheck(id, message);
-}
-
-/**
- * Checks if the ID specified matches the correct channels for eternity grinding
- * @param {string} id the ID of the message channel.
- * @param {object} message object contains everything about the message.
- * @returns {Boolean}
- */
-function eternityGrindingCheck(id, message) {
-  return earlyEternityCheck(id, message) || config.ids.ecs.includes(id) || config.ids.endgame.includes(id);
-}
-
-/**
- * Checks if the ID specified matches the correct channels for setting the crunch autobuyer
- * @param {string} id the ID of the message channel.
- * @param {object} message object contains everything about the message.
- * @returns {Boolean}
- */
-function setCrunchAutoCheck(id, message) {
-  return earlyGameCheck(id, message) || config.ids.break.includes(id);
-}
-
-/**
- * Checks if the ID specified matches the correct channels for requesting a study tree
- * @param {String} id the ID of the message channel.
- * @param {object} message object contains everything about the message.
- * @returns {Boolean}
- */
-function studytreeCheck(id, message) {
-  return ecsCheck(id, message) || earlyEternityCheck(id, message) || endgameCheck(id, message);
-}
-
-/**
- * Checks if the ID specified matches the correct channels for requesting a reality command
- * @param {String} id the ID of the message channel.
- * @param {object} message object contains everything about the message.
- * @returns {Boolean}
- */
-function e4000Check(id, message) {
-  return config.ids.endgame[1] === id || config.ids.common.includes(id) || botCommandsCheck(id, message);
-}
-
-/**
- * Checks if the ID specified matches the correct channels for requesting a command
- * @param {String} id the ID of the message channel.
- * @param {object} message object contains everything about the message.
- * @returns {Boolean}
- */
-function ecsPlusCheck(id, message) {
-  return config.ids.ecs.includes(id) || config.ids.endgame.includes(id) || config.ids.common.includes(id) || botCommandsCheck(id, message);
-}
-
-/**
- * Checks if the ID specified matches the correct channels for requesting a command
- * @param {String} id the ID of the message channel.
- * @param {object} message object contains everything about the message.
- * @returns {Boolean}
- */
-function weirdICsCheck(id, message) {
-  return config.ids.earlyEternity.includes(id) || config.ids.break.includes(id) || config.ids.common.includes(id) || botCommandsCheck(id, message);
-}
 
 /**
  * Sums all commands for the help command
@@ -653,14 +492,6 @@ function getBaseLog(x, y) {
 }
 
 module.exports = {
-  earlyGameCheck,
-  breakCheck,
-  earlyEternityCheck,
-  ecsCheck,
-  endgameCheck,
-  botCommandsCheck,
-  commonCheck,
-  e4000Check,
   constructEmbedObject,
   help,
   getMessage,
@@ -681,42 +512,26 @@ module.exports = {
     lightDark,
     generateTree
   },
-  special: {
-    bankedInfsCheck,
-    dilationGrindCheck,
-    earlyInfinityCheck,
-    eternityGrindingCheck,
-    setCrunchAutoCheck,
-    studytreeCheck,
-    ecsPlusCheck,
-    weirdICsCheck
-  },
   internal: {
     startIntervals,
     setBotStatus,
   },
   checkObject: {
-    "earlyGame": earlyGameCheck,
-    "earlyInfinity": earlyInfinityCheck,
-    // We're doing this because the way we access it in Command.js means that we access this object by using the 
-    // array entry thing (i.e. return functions.checkObject[this.check]), so we keep it here. In any other case
-    // this is an important rule to hold on to.
-    // eslint-disable-next-line object-shorthand
-    "breakCheck": breakCheck,
-    "earlyEternity": earlyEternityCheck,
-    // eslint-disable-next-line object-shorthand
-    "ecsCheck": ecsCheck,
-    "ecsPlus": ecsPlusCheck,
-    "endgame": endgameCheck,
-    "botCommands": botCommandsCheck,
-    "e4000": e4000Check,
-    "dilationGrind": dilationGrindCheck,
-    "bankedInfs": bankedInfsCheck,
-    "studyTreeCheck": studytreeCheck,
-    "eternityGrinding": eternityGrindingCheck,
-    "ic4/5": weirdICsCheck,
-    // eslint-disable-next-line object-shorthand
-    "setCrunchAutoCheck": setCrunchAutoCheck,
+    "earlyGame": checks.earlyGameCheck,
+    "earlyInfinity": checks.earlyInfinityCheck,
+    "breakCheck": checks.breakCheck,
+    "earlyEternity": checks.earlyEternityCheck,
+    "ecsCheck": checks.ecsCheck,
+    "ecsPlus": checks.ecsPlusCheck,
+    "endgame": checks.endgameCheck,
+    "botCommands": checks.botCommandsCheck,
+    "e4000": checks.e4000Check,
+    "dilationGrind": checks.dilationGrindCheck,
+    "bankedInfs": checks.bankedInfsCheck,
+    "studyTreeCheck": checks.studytreeCheck,
+    "eternityGrinding": checks.eternityGrindingCheck,
+    "ic4/5": checks.weirdICsCheck,
+    "setCrunchAutoCheck": checks.setCrunchAutoCheck,
     "true": true
   }
 };
