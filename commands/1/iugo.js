27e029d3e9271b3d7add99b24b46442f20e5380b
L4R5W <66326188+L4R5W@users.noreply.github.com>
2021-11-07 19:52:24 +0100

    I was thinking about something funny and then gave up

--- commands/1/iugo.js
+++ commands/1/iugo.js
@@ -8,6 +8,6 @@ module.exports = {
     name: "iugo",
     description: "pre break infinity upgrade order routes",
     check: "earlyInfinityCheck",
-    sent: [`https://cdn.discordapp.com/attachments/387798906333036546/883417052159836250/upgrade_order-1.jpeg`]
+    sent: [`https://cdn.discordapp.com/attachments/387798906333036546/906978039517306920/iugo_v3.jpeg`]
   })
 };
