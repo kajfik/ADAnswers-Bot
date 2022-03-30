8c1129b5066dcf3cd3bb200e38eaae211202930c
L4R5W <66326188+L4R5W@users.noreply.github.com>
2022-03-30 19:29:50 +0200

    Update androidorweb.js

--- commands/6/androidorweb.js
+++ commands/6/androidorweb.js
@@ -9,6 +9,6 @@ module.exports = {
 
     description: "sends the pinned message from the mobile channel describing the differences. Mobile and web have some notable differences -- mostly quality of life (QoL) changes. Due to this, many people prefer to play on mobile. Web will be essentially identical with the release of the Reality update as many of these changes were taken from there.",
     check: true,
-    sent: [`https://gist.github.com/earthernsence/85ba3bfd470ed6fe76944f377f91fda4`]
+    sent: [`<https://gist.github.com/earthernsence/85ba3bfd470ed6fe76944f377f91fda4>`]
   })
 };
