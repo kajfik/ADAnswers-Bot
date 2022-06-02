87f3f085a698bf1fdea85b0eb180b04a6d87928c
Mirai <93205070+Miraiah-Sozo@users.noreply.github.com>
2022-06-02 13:22:59 -0400

    Create a "save sharing" command.
    Create a "save sharing" command for the purposes of explaining to new users how to share their save with helpers. Hopefully this should eliminate most problems with it.

---
+++ commands/8/savesharing.js
@@ -0,0 +1,20 @@
+/* eslint-disable max-len */
+"use strict";
+
+const { ApplicationCommand } = require("../../classes/ApplicationCommand/ApplicationCommand");
+
+module.exports = {
+  command: new ApplicationCommand({
+
+    name: "savesharing",
+    description: "Provides a brief explanation on sharing saves.",
+    check: true,
+    sent: [`Due to the size of Antimatter Dimensions saves, you can not share them directly on Discord. \nInstead, you need to upload them to a site such as <https://paste.ee/>. Pastebin will NOT work for this. \n\nTo do this, follow these steps: 
+    1.  Export your save from the "Settings" tab in-game. 
+    2.  Open up <https://paste.ee/> in your browser of choice. 
+    3.  Paste your save data in the big box in the site. Save data begins with "AntimatterDimensionsSavefileFormat" and Ends with "EndOfSaveFile". 
+    4.  Click "Submit" at the bottom of the page. You will be taken to a new page with your save data. 
+    5.  Copy the URL to the page. It should look like this: <https://paste.ee/p/xxxxx>
+    6.  Paste that link in the Discord. Kajfik or another helper can then look at your save data.`]
+  })
+};
