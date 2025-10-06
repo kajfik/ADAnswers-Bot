38f3eb3b56bbe22b8a290f18ce74829576342267
kajfik <kajf.jakub@gmail.com>
2025-10-06 22:23:41 +0200

    Pinned discord.js version

--- src/commands/1/c9.ts
+++ src/commands/1/c9.ts
@@ -19,7 +19,7 @@ export const c9: Command = {
     if (!interaction || !interaction.isChatInputCommand()) return;
 
     const user: User = interaction.member === null ? interaction.user : interaction.member.user as User;
-    const target = interaction.options.getUser("target");
+    const target = interaction.options.getUser("target") as User;
 
     type ObjectKey = keyof typeof Challenge.newChallengeMessageObject;
     const embed: EmbedBuilder = Challenge.newChallengeMessageObject["c9" as ObjectKey] as EmbedBuilder;
